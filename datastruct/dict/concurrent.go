d12d849c4f9870c7d38d03fe9bfe893dd9b5a085
xsc <njuxushuchi@163.com>
2022-06-23 10:49:20 +0800

    开始实现dict类型

---
+++ datastruct/dict/concurrent.go
@@ -0,0 +1,80 @@
+package dict
+
+import (
+	"math"
+	"sync"
+)
+
+type ConcurrentDict struct {
+	table      []*shard
+	count      int32
+	shardCount int
+}
+
+type shard struct {
+	m     map[string]interface{}
+	mutex sync.RWMutex
+}
+
+func computeCapacity(param int) (size int) {
+	if param <= 16 {
+		return 16
+	}
+	n := param - 1
+	n |= n >> 1
+	n |= n >> 2
+	n |= n >> 4
+	n |= n >> 8
+	n |= n >> 16
+	if n < 0 {
+		return math.MaxInt32
+	}
+
+	return n + 1
+}
+
+func MakeConcurrent(shardCount int) *ConcurrentDict {
+	shardCount = computeCapacity(shardCount)
+	table := make([]*shard, shardCount)
+	for i := 0; i < shardCount; i++ {
+		table[i] = &shard{
+			m: make(map[string]interface{}),
+		}
+	}
+
+	d := &ConcurrentDict{
+		table:      table,
+		count:      0,
+		shardCount: shardCount,
+	}
+
+	return d
+}
+
+const prime32 = uint32(16777619)
+
+func fnv32(key string) uint32 {
+	hash := uint32(2166136261)
+	for i := 0; i < len(key); i++ {
+		hash *= prime32
+		hash ^= uint32(key[i])
+	}
+	return hash
+}
+
+func (dict *ConcurrentDict) spread(hashCode uint32) uint32 {
+	if dict == nil {
+		panic("dict is nil")
+	}
+	tableSize := uint32(len(dict.table))
+
+	return (tableSize - 1) & uint32(hashCode)
+}
+
+func (dict *ConcurrentDict) getShard(index uint32) *shard {
+	if dict == nil {
+		panic("dict is nil")
+	}
+
+	return dict.table[index]
+}
